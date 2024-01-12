97a21d7c9f7d972c74f99a5eed8a031c2ebc455f
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-01-13 00:59:46 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,9 @@
 const radioDropdown = document.getElementById('radio-dropdown'),
-      landingspaginaDropdown = document.getElementById('landingspagina-dropdown'),
-      dropDown1 = document.querySelector('.dropdown1'),
-      dropDown2 = document.querySelector('.dropdown2'),
-      chevron1 = document.getElementById('chevron-down1'),
-      chevron2 = document.getElementById('chevron-down2');
+      dropDown = document.querySelector('.dropdown'),
+      chevron = document.getElementById('chevron-down');
 
 radioDropdown.addEventListener('click', showDropdown1)
 function showDropdown1 () {
-    dropDown1.classList.toggle('showdropdown')
-    chevron1.classList.toggle('flipchevron')
-}
-
-landingspaginaDropdown.addEventListener('click', showDropdown2)
-function showDropdown2 () {
-    dropDown2.classList.toggle('showdropdown')
-    chevron2.classList.toggle('flipchevron')
+    dropDown.classList.toggle('showdropdown')
+    chevron.classList.toggle('flipchevron')
 }
